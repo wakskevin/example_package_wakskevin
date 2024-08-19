3ba0c597da7a7d12b197c2942086f1ab1bc1f36d
wakskevin <wakskevin@outlook.com>
2024-08-19 19:34:42 +0300

    finalized publishing to testpypi

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Overview
 
 `example_package_wakskevin` is a Python package serving as a practical example following the [Python Packaging Tutorial](https://packaging.python.org/en/latest/tutorials/packaging-projects/).
+
+## Installation
+
+```shell
+pip install -i https://test.pypi.org/simple/ example-package-wakskevin
+```
---
+++ generaterc.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from decouple import config
+
+
+def generate_rc_files():
+    # Load token from environment variables using python-decouple
+    testpypi_token = config("TESTPYPI_TOKEN")
+
+    # Define the content of the .pypirc file
+    pypirc_content = (
+        "[distutils]\n"
+        "  index-servers =\n"
+        "    testpypi\n\n"
+        "[testpypi]\n"
+        f"  username = __token__\n"
+        f"  password = {testpypi_token}\n\n"
+    )
+
+    # Get the project folder using pathlib
+    project_folder = Path(__file__).parent.resolve()
+
+    # Write the .pypirc file in the project folder
+    pypirc_path = project_folder / ".pypirc"
+    pypirc_path.write_text(pypirc_content)
+    print(f".pypirc has been generated at {pypirc_path}")
+
+
+if __name__ == "__main__":
+    generate_rc_files()
